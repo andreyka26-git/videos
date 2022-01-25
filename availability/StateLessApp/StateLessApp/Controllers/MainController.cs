f04b0661bdc0b23821d1cc32ff2c6cb37bd629af
andreyka26-git <b.andriy.b2000@gmail.com>
2022-01-26 01:31:44 +0200

    added stateful and stateless projects

---
+++ availability/StateLessApp/StateLessApp/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace StateLessApp.Controllers
+{
+    public class MainController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public MainController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("stateless-operation")]
+        public IActionResult DoStatelessOperation()
+        {
+            var nodeId = _config["NodeId"];
+
+            return Ok(nodeId);
+        }
+    }
+}
---
+++ availability/StateLessApp/StateLessApp/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace StateLessApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+            return host;
+        }
+    }
+}
